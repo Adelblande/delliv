91faf304155e3a914c733b350641aa54313de145
Adelblande <adelblande@gmail.com>
2023-12-24 02:00:08 -0300

    Jest config correction

--- frontend/src/components/atoms/Button/Button.spec.tsx
+++ frontend/src/components/atoms/Button/Button.spec.tsx
@@ -2,9 +2,9 @@ import { render } from "@testing-library/react";
 import { Button } from ".";
 
 describe("Button", () => {
-  it("Button renders correctly", async () => {
-    const { debug } = render(<Button>Entrar</Button>);
-    debug();
-    // expect(2 + 2).toBe(4);
+  it("Button renders correctly", () => {
+    const { getByText } = render(<Button>Entrar</Button>);
+
+    expect(getByText("Entrar")).toBeInTheDocument();
   });
 });
